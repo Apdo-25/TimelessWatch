f19e7d2108a99688cf0dee849f36b9ef526f08c9
Apdo-25 <abdo0062@easv365.dk>
2023-11-21 20:39:33 +0100

    dummy data to home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Container from "./components/Container";
 import HeroBanner from "./components/HeroBanner";
+import {products} from "@/utils/data"
 
 export default function Home() {
   return (
@@ -7,6 +8,11 @@ export default function Home() {
       <Container>
         <div>
           <HeroBanner/>
+        </div>
+        <div>
+          {products.map((product: any) => {
+            return <div key={product.id}> {product.name} </div>
+          })}
         </div>
       </Container>
     </div>
