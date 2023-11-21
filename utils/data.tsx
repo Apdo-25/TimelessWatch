bd48ca47ffb4977d29fb916059e98401c7c12a8d
Apdo-25 <abdo0062@easv365.dk>
2023-11-21 20:28:23 +0100

    dummydata

---
+++ utils/data.tsx
@@ -0,0 +1,158 @@
+export const products = [
+  {
+    id: "123",
+    name: "Apple Watch",
+    price: 1000,
+    description: "This is a Apple watch",
+    category: "Watch",
+    brand: "Apple",
+    inStock: true,
+    images: [
+      {
+        color: "gray",
+        colorCode: "#d1d5db",
+        image: "/images/rolex.jpg",
+      },
+      {
+        color: "black",
+        colorCode: "#111827",
+        image: "/images/rolex.jpg",
+      },
+      {
+        color: "white",
+        colorCode: "#ffffff",
+        image: "/images/rolex.jpg",
+      },
+    ],
+    reviews: [],
+  },
+    {
+        id: "124",
+        name: "Rolex Watch",
+        price: 1000,
+        description: "This is a Rolex watch",
+        category: "Watch",
+        brand: "Rolex",
+        inStock: true,
+        images: [
+        {
+            color: "gray",
+            colorCode: "#d1d5db",
+            image: "/images/rolex.jpg",
+        },
+        {
+            color: "black",
+            colorCode: "#111827",
+            image: "/images/rolex.jpg",
+        },
+        {
+            color: "white",
+            colorCode: "#ffffff",
+            image: "/images/rolex.jpg",
+        },
+        ],
+        reviews: [],
+    },
+    {
+        id: "125",
+        name: "Omega Watch",
+        price: 1000,
+        description: "This is a Omega watch",
+        category: "Watch",
+        brand: "Omega",
+        inStock: true,
+        images: [
+        {
+            color: "gray",
+            colorCode: "#d1d5db",
+            image: "/images/rolex.jpg",
+        },
+        {
+            color: "black",
+            colorCode: "#111827",
+            image: "/images/rolex.jpg",
+        },
+        {
+            color: "white",
+            colorCode: "#ffffff",
+            image: "/images/rolex.jpg",
+        },
+        ],
+        reviews: [],
+    },
+    {
+        id: "126",
+        name: "Casio Watch",
+        price: 1000,
+        description: "This is a Casio watch",
+        category: "Watch",
+        brand: "Casio",
+        inStock: true,
+        images: [
+        {
+            color: "gray",
+            colorCode: "#d1d5db",
+            image: "/images/rolex.jpg",
+        },
+        {
+            color: "black",
+            colorCode: "#111827",
+            image: "/images/rolex.jpg",
+        },
+        {
+            color: "white",
+            colorCode: "#ffffff",
+            image: "/images/rolex.jpg",
+        },
+        ],
+        reviews: [],
+    },
+    {
+        id: "127",
+        name: "Patek Philippe Watch",
+        price: 1000,
+        description: "This is a Patek Philippe watch",
+        category: "Watch",
+        brand: "Patek Philippe",
+        inStock: true,
+        images: [
+        {
+            color: "gray",
+            colorCode: "#d1d5db",
+            image: "/images/rolex.jpg",
+        },
+        {
+            color: "black",
+            colorCode: "#111827",
+            image: "/images/rolex.jpg",
+        },
+        {
+            color: "white",
+            colorCode: "#ffffff",
+            image: "/images/rolex.jpg",
+        },
+        ],
+        reviews: [
+            {
+                id: "1",
+                userId: "1",
+                productId: "1",
+                rating: 5,
+                comment: "best watch eveeer!!",
+                createdDate: "2023-07-06T06:08:33.067Z",
+                user: {
+                    id: "1",
+                    name: "king kong",
+                    email: "king@king.com",
+                    emailVerified: null,
+                    image: null,
+                    hashedPassword: null,
+                    createdAt: "2023-07-06T06:08:33.067Z",
+                    updatedAt:"2023-07-06T06:08:33.067Z",
+                    role: "Admin",
+                }
+
+            }
+        ],
+    },
+];
