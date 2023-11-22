52a841b2a0deee86aab5644d14f4ab7eab72e353
Apdo-25 <abdo0062@easv365.dk>
2023-11-22 22:40:27 +0100

    Update useCart.tsx

--- hooks/useCart.tsx
+++ hooks/useCart.tsx
@@ -1,27 +1,46 @@
-import { createContext, useContext, useState } from "react";
+import { CartProductType } from "@/app/product/[productId]/ProductDetails";
+import { createContext, useCallback, useContext, useState } from "react";
 
 type CartContextType = {
-    cartTotalQty: number
+    cartTotalQty: number;
+    cartProducts: CartProductType;
+    handleAddProductToCart: (product: CartProductType) => void
 }
 
 export const CartContext = createContext<CartContextType | null>(null);
 
-type Props = {
+export interface Props {
     [propName: string]: any;
 }
 
 export const CartContextProvider = (props: Props) => {
-
     const [cartTotalQty, setCartTotalQty] = useState(0)
+    const [cartProducts, setCartProducts] = useState<CartProductType[] | null>(null);
+
+    const handleAddProductToCart = useCallback((product: CartProductType) => {
+        setCartProducts((prev) => {
+            let updatedCart;
+
+            if(prev) {
+                updatedCart = [...prev, product]
+            }else {
+                updatedCart = [product]
+            }
+
+            return updatedCart
+        })
+    }, []);
 
     const value = {
-        cartTotalQty
+        cartTotalQty,
+        cartProducts,
+        handleAddProductToCart,
     }
     return <CartContext.Provider value={value} {...props}/>
 }
 
 export const useCart = () => {
-    const context = useContext(CartContext)
+    const context = useContext(CartContext);
 
     if(context === null ) {
         throw new Error("useCart must be used within a CartContextProvider")
