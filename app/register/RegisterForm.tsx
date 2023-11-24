da96994719bf32a1c232b4b836c5181e14ff675e
Apdo-25 <abdo0062@easv365.dk>
2023-11-24 18:05:15 +0100

    register page and form

---
+++ app/register/RegisterForm.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import { useState } from "react";
+import Heading from "../components/Heading";
+import Input from "../components/inputs/Input";
+import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
+
+const RegisterForm = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FieldValues>({
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  return (
+    <>
+      <Heading title="Sign up for Timeless~Watch" />
+      <hr className="bg-slate-300 w-full h-px" />
+      <Input
+        id="name"
+        label="Name"
+        type="text"
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
+      />
+    </>
+  );
+};
+
+export default RegisterForm;
