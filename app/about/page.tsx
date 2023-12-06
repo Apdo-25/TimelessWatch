32f80527f91d1dd8cc7cacd989b0944b103bb37f
Apdo-25 <abdo0062@easv365.dk>
2023-12-06 17:44:24 +0100

    fix

---
+++ app/about/page.tsx
@@ -0,0 +1,11 @@
+import Heading from "../components/Heading";
+
+const AboutPage = () => {
+  return (
+    <div>
+      <Heading title="About us " center />
+    </div>
+  );
+};
+
+export default AboutPage;
---
+++ app/contact/page.tsx
@@ -0,0 +1,11 @@
+import Heading from "../components/Heading";
+
+const ContactPage = () => {
+  return (
+    <div>
+      <Heading title="Contact Us" center />
+    </div>
+  );
+};
+
+export default ContactPage;
